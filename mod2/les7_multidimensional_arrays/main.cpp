6b931f49fa9fd6c8e59ae28ccefb7ea42182419e
Alexandr Kravtsov <ak@4xxi.com>
2017-01-27 20:19:01 +0300

    WIP

--- mod2/les7_multidimensional_arrays/main.cpp
+++ mod2/les7_multidimensional_arrays/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdlib.h>
 
 using namespace std;
 
@@ -6,6 +7,15 @@ int **create_array_2d(size_t a, size_t b) {
     int **m = new int *[a];
     for (size_t i = 0; i != a; ++i)
         m[i] = new int[b];
+
+    return m;
+}
+
+int **create_array_2d_effective(size_t a, size_t b) {
+    int **m = new int *[a];
+    m[0] = new int [a * b];
+    for (int i = 1; i != a; ++i)
+        m[i] = m[i-1] + b;
 
     return m;
 }
@@ -18,34 +28,141 @@ void free_array_2d(int **m, size_t a, size_t b) {
     delete[] m;
 }
 
+void free_array_2d_effective(int **m) {
+    delete [] m[0];
+    delete [] m;
+}
+
+int ** transpose(const int * const * m, unsigned rows, unsigned cols)
+{
+    int ** transposed = create_array_2d_effective(cols, rows);
+
+    for (int j = 0; j < cols; j++) {
+        for (int i = 0; i < rows; i++) {
+            transposed[j][i] = m[i][j];
+        }
+    }
+
+    return transposed;
+}
+
+void swap_min(int *m[], unsigned rows, unsigned cols)
+{
+    int min = m[0][0];
+    unsigned min_row_n = 0;
+
+    for (std::size_t i = 0; i != (rows - 1); i++) {
+        for (std::size_t j = 0; j != (rows -1); j++) {
+            if (m[i][j] < min) {
+                min = m[i][j];
+                min_row_n = (unsigned int) i;
+            }
+        }
+    }
+
+    cout << min << " " << min_row_n;
+
+}
+
 int main() {
 
-    int ** m2d2 = create_array_2d(10, 10);
+    int **matrix = create_array_2d_effective(2,3);
+    matrix[0][0] = 2;
+    matrix[0][1] = 4;
+    matrix[0][2] = 5;
 
-    for (size_t i = 0; i != 10; i++) {
-        for (size_t j = 0; j != 10; j++) {
-            cout << m2d2[i][j] << ' ';
-        }
-        cout << endl;
-    }
+    matrix[1][0] = 1;
+    matrix[1][1] = 2;
+    matrix[1][2] = 2;
 
-    int m2d[2][3] = {{1, 2, 3},
-                     {4, 5, 6}};
+    swap_min(matrix, 3, 3);
 
-    for (size_t i = 0; i != 2; i++) {
-        for (size_t j = 0; j != 3; j++) {
-            cout << m2d[i][j] << ' ';
-        }
-        cout << endl;
-    }
 
-    // Как выделяется под него хип
-    // Массив из 20 элементов 4 х 5
-    int **m2d1 = new int *[5];
+//    int **matrix = create_array_2d_effective(2,3);
+//    matrix[0][0] = 1;
+//    matrix[0][1] = 1;
+//    matrix[0][2] = 1;
+//
+//    matrix[1][0] = 2;
+//    matrix[1][1] = 2;
+//    matrix[1][2] = 2;
+//
+//    for (size_t i = 0; i != 2; i++) {
+//        for (size_t j = 0; j != 3; j++) {
+//            cout << matrix[i][j] << ' ';
+//        }
+//        cout << endl;
+//    }
+//
+//    cout << endl;
+//    matrix[2][0] = 3;
+//    matrix[2][1] = 3;
+//    matrix[2][2] = 3;
 
-    for (size_t i = 0; i != 5; ++i) {
-        m2d1[i] = new int[4];
-    }
+//    int ** t = transpose((const int *const *) matrix, 2, 3);
+//
+//    for (size_t i = 0; i != 3; i++) {
+//        for (size_t j = 0; j != 2; j++) {
+//            cout << t[i][j] << ' ';
+//        }
+//        cout << endl;
+//    }
+
+//    for (size_t i = 0; i != 3; i++) {
+//        for (size_t j = 0; j != 2; j++) {
+//            cout << matrix[i][j] << ' ';
+//        }
+//        cout << endl;
+//    }
+//
+//    cout << endl;
+//
+//    for (size_t i = 0; i != 3; i++) {
+//        for (size_t j = 0; j != 2; j++) {
+//            cout << t[i][j] << ' ';
+//        }
+//        cout << endl;
+//    }
+
+
+//    *(t[0]) = ;
+//    t[0][1] = 1;
+//    t[0][2] = 1;
+
+    // Как эффективно выделить память под масив 5 на 4. т е 20 ячеек
+    // Вот так пёс
+//    int ** m = new int * [5];
+//    m[0] = new int [5 * 4];
+//    for (size_t i = 0; i != 5; ++i) {
+//        m[i] = m[i - 1] + 4;
+//    }
+//
+//    int ** m2d2 = create_array_2d(10, 10);
+//
+//    for (size_t i = 0; i != 10; i++) {
+//        for (size_t j = 0; j != 10; j++) {
+//            cout << m2d2[i][j] << ' ';
+//        }
+//        cout << endl;
+//    }
+//
+//    int m2d[2][3] = {{1, 2, 3},
+//                     {4, 5, 6}};
+//
+//    for (size_t i = 0; i != 2; i++) {
+//        for (size_t j = 0; j != 3; j++) {
+//            cout << m2d[i][j] << ' ';
+//        }
+//        cout << endl;
+//    }
+//
+//    // Как выделяется под него хип
+//    // Массив из 20 элементов 4 х 5
+//    int **m2d1 = new int *[5];
+//
+//    for (size_t i = 0; i != 5; ++i) {
+//        m2d1[i] = new int[4];
+//    }
 
 
     return 0;
